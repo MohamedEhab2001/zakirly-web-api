f32ad2eeb75a9ab77907aca02ce8ac8da05dd8cb
Mohamed Ehab <ahmedhossam@EGEGY-MM-016.local>
2026-02-07 02:25:40 +0200

    test

--- repositories/WebContent/index.js
+++ repositories/WebContent/index.js
@@ -5,7 +5,14 @@ const ModelValidation = require("../../helpers/Validations/ModelValidation");
 class WebContentRepository {
   static async create(data) {
     const modelValidate = new ModelValidation(models.WebContent);
-    modelValidate.ModelKeysValidate(data, []);
+    modelValidate.ModelKeysValidate(data, [
+    "video_url",
+    "video_thumbnail",
+    "herosection_title",
+    "hersection_slogan",
+    "herosection_description",
+    "hersection_image_url"
+    ]);
     return await models.WebContent.create(data);
   }
 
