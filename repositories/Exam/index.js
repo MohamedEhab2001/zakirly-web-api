70e7a6f39663c107921a2c3c046d1f914b0981eb
mohamedehab2001 <dracolla.p@gmail.com>
2026-03-28 20:06:25 +0200

    deletion

--- repositories/Exam/index.js
+++ repositories/Exam/index.js
@@ -1,7 +1,7 @@
 
 const models = require("../../models");
 const ModelValidation = require("../../helpers/Validations/ModelValidation");
-const { All , One, ExamQuestion } = require("./Shapes");
+const { All, One, ExamQuestion } = require("./Shapes");
 const { extractKeys } = require("../../helpers/HelperMethods");
 
 class ExamRepository {
@@ -9,30 +9,34 @@ class ExamRepository {
     const transaction = await models.sequelize.transaction()
     const modelValidate = new ModelValidation(models.Exam);
     modelValidate.ModelKeysValidate(data, []);
-    const record = await models.Exam.create(data , {transaction});
-    return [record , transaction]
+    const record = await models.Exam.create(data, { transaction });
+    return [record, transaction]
   }
 
   static async getAll(data) {
-    const [country] = extractKeys(data,["country"])
+    const [country] = extractKeys(data, ["country"])
 
-    const get = All(models , country || "eg")
-    return await models.Exam.findAll({...get});
+    const get = All(models, country || "eg")
+    return await models.Exam.findAll({ ...get });
   }
 
-  static async ById(id , data) {
-    const [country] = extractKeys(data,["country"])
-    const get = One(models , country || "eg")
-    return await models.Exam.findByPk(id , {...get});
+  static async ById(id, data) {
+    const [country] = extractKeys(data, ["country"])
+    const get = One(models, country || "eg")
+    return await models.Exam.findByPk(id, { ...get });
   }
 
-  static async ExamQuestionById(id ) {
+  static async ExamQuestionById(id) {
     const get = ExamQuestion(models)
-    return await models.Exam.findByPk(id , {...get});
+    return await models.Exam.findByPk(id, { ...get });
   }
 
   static async update(id, data) {
     return await models.Exam.update(data, { where: { id } });
+  }
+
+  static async delete(id) {
+    return await models.Exam.destroy({ where: { id } });
   }
 }
 
